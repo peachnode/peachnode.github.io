b3fc0a3cdd2e6b478ce295ae514a178278137000
Ingunn Augdal Fløvig <ingunn.flovig@gmail.com>
2023-06-29 17:13:05 +0200

    added resizing of the logo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="home">
-    <img
-      alt="peach hub logo"
-      src="../assets/logo-transparent.png"
-      class="logo"
-    />
-    <HeadLine msg="Welcome to our website" />
+    <div class="container">
+      <img
+        alt="peach hub logo"
+        src="../assets/logo-transparent.png"
+        class="logo"
+      />
+      <HeadLine msg="Welcome to our website" />
+    </div>
   </div>
 </template>
 
@@ -22,8 +24,29 @@ export default defineComponent({
 </script>
 
 <style scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.container {
+  width: 30vw;
+  height: auto;
+  margin: 0 auto;
+  padding: 20px;
+}
+
 .logo {
-  width: 500px; /* Set the width of the logo */
-  /*height: 100px; !* Set the height of the logo *!*/
+  width: 100%; /* Set the maximum width of the logo to 100% of its container */
+  height: auto; /* Allow the logo to scale proportionally */
+  /*transition: max-width 0.5s;*/
+}
+
+@media screen and (max-width: 600px) {
+  .logo {
+    width: 80%; /* Reduce the logo's max-width for smaller screens */
+  }
 }
 </style>
