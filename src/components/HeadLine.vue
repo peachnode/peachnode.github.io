0b5d3ae04d0c9207ad0462404d2ac63d5353a64c
lynn-janzen007 <lynn.janzen@campus.tu-berlin.de>
2023-06-29 15:43:42 +0200

    rename component and add logo

--- src/components/HeadLine.vue
+++ src/components/HeadLine.vue
@@ -8,7 +8,7 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "HelloWorld",
+  name: "headline-item",
   props: {
     msg: String,
   },
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <img alt="peach hub logo" src="../assets/logo.png" class="logo" />
+    <HeadLine msg="Welcome to our website" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import HeadLine from "@/components/HeadLine.vue"; // @ is an alias to /src
 
 export default defineComponent({
   name: "HomeView",
   components: {
-    HelloWorld,
+    HeadLine,
   },
 });
 </script>
+
+<style scoped>
+.logo {
+  width: 500px; /* Set the width of the logo */
+  /*height: 100px; !* Set the height of the logo *!*/
+}
+</style>
