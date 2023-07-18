3c2ecb91ab19cf8db688a7414b26f4ac4cc2f76e
nao <contact@peachno.de>
2023-07-18 12:21:20 +0200

    change vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/peach-hub-website/" : "/",
-});
+// const { defineConfig } = require("@vue/cli-service");
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   publicPath:
+//     process.env.NODE_ENV === "production" ? "/peach-hub-website/" : "/",
+// });
+module.exports = {
+  publicPath: "/peachnode.github.io/",
+};
